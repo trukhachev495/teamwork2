fb5e6eece05a8c47f43a8579424e5108c03e758d
trukhachev495 <sergey.trainings@gmail.com>
2022-07-15 19:59:33 +0300

    Revert "исправил метод addPlayTim,fixed #2"
    This reverts commit 5fc731b1d29b345fb5d3f4a82a629494ebdaa898.

--- src/main/java/ru/netology/GameStore.java
+++ src/main/java/ru/netology/GameStore.java
@@ -44,11 +44,8 @@ public class GameStore {
      * суммироваться с прошлым значением для этого игрока
      */
     public void addPlayTime(String playerName, int hours) {
-        if (hours <= 0) {
-            throw new RuntimeException("Отрицательное или нулевое игровое время невалидно!");
-        }
         if (playedTime.containsKey(playerName)) {
-            playedTime.put(playerName, playedTime.get(playerName) + hours);
+            playedTime.put(playerName, playedTime.get(playerName));
         } else {
             playedTime.put(playerName, hours);
         }
